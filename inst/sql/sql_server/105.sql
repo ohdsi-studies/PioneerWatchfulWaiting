072dcd0894178068e2fb85db4286d1cfacb1a62f
Maxim Moinat <maxim@thehyve.nl>
2021-06-18 15:51:21 +0200

    Update inst/sql/sql_server/105.sql

--- inst/sql/sql_server/105.sql
+++ inst/sql/sql_server/105.sql
@@ -877,6 +877,11 @@ from
   FROM @cdm_database_schema.OBSERVATION o
 JOIN #Codesets codesets on ((o.observation_concept_id = codesets.concept_id and codesets.codeset_id = 18))
 ) C
+{@episodetable} ? {  union all
+  select ep.person_id, ep.episode_id, ep.episode_start_datetime, ep.episode_end_datetime,ep.episode_object_concept_id,null
+  from @cdm_database_schema.episode ep
+JOIN #Codesets codesets on ((ep.episode_object_concept_id = codesets.concept_id and codesets.codeset_id = 18))
+}) C
 
 WHERE C.value_as_number > 10.0000
 -- End Observation Criteria
