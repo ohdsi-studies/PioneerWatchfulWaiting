048790809bf3496f2f3e50ef4cb491fe99c74326
keesvanbochove <kees@thehyve.nl>
2021-08-18 18:09:13 +0200

    Fix for issue #77

--- R/RunStudy.R
+++ R/RunStudy.R
@@ -380,7 +380,7 @@ runStudy <- function(connectionDetails = NULL,
   ParallelLogger::logInfo("********************************************************************************************")
   # Ensure that the covariate_value.csv is free of any duplicative values. This can happen after more than
   # one run of the package.
-  cv <- data.table::fread_csv(file.path(exportFolder, "covariate_value.csv"), col_types = readr::cols())
+  cv <- data.table::fread(file.path(exportFolder, "covariate_value.csv"))
   cv <- unique(cv)
   writeToCsv(cv, file.path(exportFolder, "covariate_value.csv"), incremental = FALSE)
   
